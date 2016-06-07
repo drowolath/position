6ff5d2a6e8c60c62fea084901f119c5a452144ed
Thomas Ayih-Akakpo <thomas.ayih-akakpo@gulfsat.mg>
2016-06-07 14:18:24 +0300

    modifie la version de leaflet

--- position/static/js/position.js
+++ position/static/js/position.js
@@ -42,7 +42,7 @@ function getRealTimePosition(device, layer) {
 
 /* Initialisation du fond de carte et de tous les attributs liés */
 var liveLayer = new L.layerGroup();
-var osmLayer = L.tileLayer("http://{s}.tile.osm.org/{z}/{x}/{y}.png", {
+var osmLayer = L.tileLayer("http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
     MaxZoom: 23,
     subdomains:["a", "b", "c"],
     attribution: "Gulfsat Madagascar"
